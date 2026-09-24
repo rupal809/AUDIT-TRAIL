6f166342b6ac654c617b07d2dcf16b0483ab1e21
Gauri <pandeygauri8887@gmail.com>
2026-09-24 23:36:48 +0530

    Changes in dashboard

--- src/services/dashboardService.js
+++ src/services/dashboardService.js
@@ -1,16 +1,16 @@
 import { apiRequest } from "./api";
 
-// to fetch dashboard stats from the backend
+// to fetch dashboard stats 
 export const getDashboardStats = async () => {
   return apiRequest("/dashboard/stats");
 };
 
-// to fetch recent events from the backend
+// to fetch recent events 
 export const getRecentEvents = async () => {
   return apiRequest("/dashboard/events");
 };
 
-// to fetch delayed containers from the backend
+// to fetch delayed containers 
 export const getDelayedContainers = async () => {
   return apiRequest("/dashboard/delayed-containers");
 };
